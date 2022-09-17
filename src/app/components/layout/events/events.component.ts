028b954ff152a1c3df8f02fd96ec3d8a4258d095
MaryamMakkawi <83539247+MaryamMakkawi@users.noreply.github.com>
2022-09-17 19:28:18 +0300

    events component

--- src/app/components/layout/events/events.component.ts
+++ src/app/components/layout/events/events.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-events',
   templateUrl: './events.component.html',
-  styleUrls: ['./events.component.scss']
+  styleUrls: ['./events.component.scss'],
 })
 export class EventsComponent implements OnInit {
-
-  constructor() { }
+  eventAll!: any[];
+  constructor(private api: ApiService) {}
 
   ngOnInit(): void {
+    // All Article
+    this.api
+      .post('http://localhost/aphamea_project/web/index.php/activity/get-all', {
+        type: 2,
+        searchFilters: {
+          filters: [
+            { name: 'title', status: false },
+            { name: 'content', status: false },
+          ],
+          searchText: '',
+          platform: 0,
+        },
+      })
+      .subscribe({
+        next: (res: any) => {
+          if (res.status == 'ok') {
+            this.eventAll = res.activities;
+            console.log(this.eventAll);
+          }
+        },
+      });
   }
-
 }
